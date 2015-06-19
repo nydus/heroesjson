a730bd5043dcaadd895f7765dd54d70879297ec5
Robert Schultz <robert@cosmicrealms.com>
2015-06-19 17:28:28 -0400

    talents wip

--- generate.js
+++ generate.js
@@ -6,10 +6,12 @@ var base = require("xbase"),
 	runUtil = require("xutil").run,
 	fileUtil = require("xutil").file,
 	moment = require("moment"),
-	skeemas = require("skeemas"),
+	jsen = require("jsen"),
+	jsonselect = require("JSONSelect"),
 	libxmljs = require("libxmljs"),
 	C = require("C"),
 	rimraf = require("rimraf"),
+	jp = require("jsonpath"),
 	tiptoe = require("tiptoe");
 
 if(process.argv.length<3 || !fs.existsSync(process.argv[2]))
@@ -86,7 +88,7 @@ tiptoe(
 	},*/
 	function loadDataAndSaveJSON()
 	{
-		var heroDocs = [];
+		var xmlDocs = [];
 
 		base.info("Loading data...");
 		NEEDED_FILE_PATHS.forEach(function(NEEDED_FILE_PATH)
@@ -104,13 +106,16 @@ tiptoe(
 					S[line.substring(0, line.indexOf("="))] = line.substring(line.indexOf("=")+1).trim();
 				});
 			}
-			else if(diskPath.endsWith("HeroData.xml") || diskPath.endsWith("ZagaraData.xml"))
+			else if(diskPath.endsWith(".xml"))
 			{
-				heroDocs.push(libxmljs.parseXml(fileData));
+				xmlDocs.push(libxmljs.parseXml(fileData));
 			}
 		});
 
-		var heroes = getHeroNodes(heroDocs).map(processHeroNode);
+		var heroNodeMap = getMergedNodeMap(xmlDocs, "CHero", "Random");
+		var talentNodeMap = getMergedNodeMap(xmlDocs, "CTalent");
+		var behaviorNodeMap = getMergedNodeMap(xmlDocs, "CBehaviorBuff");
+		var heroes = Object.values(heroNodeMap).map(function(heroNode) { return processHeroNode(heroNode, talentNodeMap, behaviorNodeMap); });
 
 		base.info("Validating %d heroes...", heroes.length);
 		heroes.forEach(validateHero);
@@ -133,46 +138,55 @@ tiptoe(
 	}
 );
 
-function getHeroNodes(heroDocs)
+function getMergedNodeMap(xmlDocs, nodeName, ignoredids)
 {
-	var heroNodes = {};
+	var nodes = {};
+	ignoredids = ignoredids || [];
 
-	heroDocs.forEach(function(heroDoc)
+	xmlDocs.forEach(function(xmlDoc)
 	{
-		heroDoc.find("/Catalog/CHero").forEach(function(heroNode)
+		xmlDoc.find("/Catalog/" + nodeName).forEach(function(node)
 		{
-			if(!heroNode.attr("id") || heroNode.childNodes().length===0)
+			if(!node.attr("id") || node.childNodes().length===0)
 				return;
 
-			var heroid = attributeValue(heroNode, "id");
-			if(heroid==="Random")
+			var nodeid = attributeValue(node, "id");
+			if(ignoredids.contains(nodeid))
 				return;
 
-			if(!heroNodes.hasOwnProperty(heroid))
+			if(!nodes.hasOwnProperty(nodeid))
 			{
-				heroNodes[heroid] = heroNode;
+				nodes[nodeid] = node;
 				return;
 			}
 
-			mergeXML(heroNode, heroNodes[heroid]);
+			mergeXML(node, nodes[nodeid]);
 		});
 	});
 
-	return Object.values(heroNodes);
+	return nodes;
 }
 
 function mergeXML(fromNode, toNode)
 {
 	fromNode.childNodes().forEach(function(childNode)
 	{
+		if(childNode.name()==="TalentTreeArray")
+		{
+			var existingChildNode = toNode.get("TalentTreeArray[@Tier='" + attributeValue(childNode, "Tier") + "' and @Column='" + attributeValue(childNode, "Column") + "']");
+			if(existingChildNode)
+				existingChildNode.remove();
+		}
+
 		toNode.addChild(childNode);
 	});
 }
 
-function processHeroNode(heroNode)
+function processHeroNode(heroNode, talentNodeMap, behaviorNodeMap)
 {
 	var hero = {};
 
+	// Core hero data
 	hero.id = heroNode.attr("id").value();
 	hero.name = S["Unit/Name/" + getValue(heroNode, "Unit", "Hero" + hero.id)];
 	hero.title =  S["Hero/Title/" + hero.id];
@@ -206,8 +220,80 @@ function processHeroNode(heroNode)
 	if(releaseDateNode)
 		hero.releaseDate = moment(attributeValue(releaseDateNode, "Month", 1) + "-" + attributeValue(releaseDateNode, "Day", 1) + "-" + attributeValue(releaseDateNode, "Year", "2014"), "M-D-YYYY").format("YYYY-MM-DD");
 
-    performHeroModifications(hero);
+	// Talents
+	hero.talents = {};
+	C.HERO_TALENT_LEVELS.forEach(function(HERO_TALENT_LEVEL) { hero.talents[HERO_TALENT_LEVEL] = []; });
+	heroNode.find("TalentTreeArray").forEach(function(talentTreeNode)
+	{
+		var talent = {};
 
+		if(!!attributeValue(talentTreeNode, "removed"))
+			return;
+
+		talent.id = attributeValue(talentTreeNode, "Talent");
+
+		var talentNode = talentNodeMap[talent.id];
+		var faceid = getValue(talentNode, "Face");
+
+		talent.description = S["Button/Tooltip/" + faceid];
+
+		if(!talent.description && faceid==="TyrandeHuntersMarkTrueshotAuraTalent")
+			talent.description = S["Button/Tooltip/TyrandeTrueshotBowTalent"];
+
+		if(!talent.description)
+		{
+			base.warn("Missing talent description for hero [%s] and talentid [%s] and faceid [%s]", hero.id, talent.id, faceid);
+			return;
+		}
+
+		talent.name = talent.description.replace(/<s val="StandardTooltipHeader">([^<]+)<\/s>.+/, "$1");
+		talent.description = talent.description.replace(/<s val="StandardTooltipHeader">[^<]+<\/s>(.+)/, "$1").replace(/<s val="StandardTooltip">?(.+)/, "$1");
+		if(talent.description.indexOf("<n/>")===0)
+			talent.description = talent.description.replace(/(?:<n\/>)+(.+)/, "$1");
+
+		//talent.name = S["Button/Name/" + faceid];
+
+		if(talent.id==="NovaCombatStyleOneintheChamber")
+		{
+			var dynamics = talent.description.match(/<d ref="[^"]+"\/>/g);
+			dynamics.forEach(function(dynamic)
+			{
+				base.info(dynamic);
+				var formula = dynamic.replace(/<d ref="([^"]+)"\/>/, "$1");
+
+				var result = "";
+
+				talent.description = talent.description.replace(dynamic, result);
+			});
+
+			base.info(talent.description);
+			//100*Behavior,NovaOneintheChamber,Modification.DamageDealtFraction[Ranged]
+			
+			//<d ref="100*Behavior,NovaOneintheChamber,Modification.DamageDealtFraction[Ranged]"/>
+		}
+
+		/*<CBehaviorBuff id="NovaOneintheChamber">
+        <Alignment value="Positive"/>
+        <Modification>
+            <DamageDealtFraction index="Ranged" value="0.8"/>
+        </Modification>
+        <DamageResponse Chance="1" Handled="NovaOneintheChamberRemoveBehavior" Location="Attacker">
+            <Kind index="Spell" value="0"/>
+            <Kind index="Melee" value="0"/>
+            <Kind index="NoProc" value="0"/>
+        </DamageResponse>
+        <InfoIcon value="Assets\Textures\storm_btn-extra_int_0.dds"/>
+        <Duration value="3"/>
+    </CBehaviorBuff>
+
+ */
+
+		hero.talents[C.HERO_TALENT_LEVELS[((+attributeValue(talentTreeNode, "Tier"))-1)]].push(talent);
+	});
+	
+	// Final modifications
+    performHeroModifications(hero);
+	
 	return hero;
 }
 
@@ -218,36 +304,24 @@ function performHeroModifications(hero)
 
 	C.HERO_MODIFICATIONS[hero.id].forEach(function(HERO_MODIFICATION)
 	{
-		if(HERO_MODIFICATION.set)
+		var match = jsonselect.match(HERO_MODIFICATION.path, hero);
+		if(!match || match.length<1)
 		{
-			Object.forEach(HERO_MODIFICATION.set, function(key, value)
-			{ 
-				if(Object.isObject(value))
-				{
-					if(!hero.hasOwnProperty(key))
-						hero[key] = {};
-
-					Object.forEach(value, function(newField, newFieldValue)
-					{
-						hero[key][newField] = newFieldValue;
-					});
-				}
-				else
-				{
-					hero[key] = value; 
-				}
-			});
+			base.error("Failed to match [%s] to: %s", HERO_MODIFICATION.path, hero);
+			return;
 		}
+
+		match[0][HERO_MODIFICATION.name] = HERO_MODIFICATION.value;
 	});
 }
 
 function validateHero(hero)
 {
-	var result = skeemas.validate(hero, C.HERO_JSON_SCHEMA);
-	if(!result.valid)
+	var validator = jsen(C.HERO_JSON_SCHEMA);
+	if(!validator(hero))
 	{
 		base.warn("Hero %s (%s) has FAILED VALIDATION", hero.id, hero.name);
-		base.warn(result.errors);
+		base.info(validator.errors);
 	}
 }
 
--- shared/C.js
+++ shared/C.js
@@ -1,5 +1,21 @@
 "use strict";
 
+var base = require("xbase"); // jshint ignore:line
+
+exports.HERO_MODIFICATIONS =
+{
+	"Crusader" :
+	[
+		{ path : ":root", name : "ratings", value : { damage : 3, utility : 6, survivability : 10, complexity : 4 } }
+	],
+	"Chen" :
+	[
+		{ path : ":root", name : "releaseDate", value : "2014-09-10" }
+	]
+};
+
+exports.HERO_TALENT_LEVELS = [1, 4, 7, 10, 13, 16, 20];
+
 exports.HERO_JSON_SCHEMA =
 {
 	name : "hero",
@@ -7,14 +23,15 @@ exports.HERO_JSON_SCHEMA =
 	additionalProperties : false,
 	properties :
 	{
-		id          : { type : "string" },
-		name        : { type : "string" },
-		title       : { type : "string" },
+		id          : { type : "string", minLength : 1 },
+		name        : { type : "string", minLength : 1 },
+		title       : { type : "string", minLength : 1 },
 		role        : { type : "string", enum : ["Assassin", "Warrior", "Support", "Specialist"] },
 		type        : { type : "string", enum : ["Melee", "Ranged"] },
 		gender      : { type : "string", enum : ["Female", "Male"] },
-		franchise   : { type : "string" },
+		franchise   : { type : "string", minLength : 1 },
 		difficulty  : { type : "string", enum : ["Easy", "Medium", "Hard", "Very Hard"] },
+		releaseDate : { type : "string", pattern : "2[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]" },
 		ratings     :
 			{
 				type : "object",
@@ -28,22 +45,39 @@ exports.HERO_JSON_SCHEMA =
 					complexity    : { type : "integer", minimum : 1, maximum : 10 }
 				}
 			},
-		releaseDate : { type : "string", pattern : "2[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]" }
+		talents :
+			{
+				type : "object",
+				additionalProperties : false,
+				properties : {}
+			}
 	}
 };
+
+var HERO_TALENT_TIER_JSON_SCHEMA =
+{
+	type : "array",
+	minItems : 3,
+	maxItems : 5,
+	items :
+		{
+			type : "object",
+			additionalProperties : false,
+			required : ["id", "name", "description"],
+			properties :
+			{
+				id          : { type : "string", minLength : 1 },
+				name        : { type : "string", minLength : 1 },
+				description : { type : "string", minLength : 1 }
+			}
+		}
+};
+
+exports.HERO_TALENT_LEVELS.forEach(function(HERO_TALENT_LEVEL) { exports.HERO_JSON_SCHEMA.properties.talents.properties[HERO_TALENT_LEVEL] = base.clone(HERO_TALENT_TIER_JSON_SCHEMA, true); });
+exports.HERO_JSON_SCHEMA.properties.talents.required = exports.HERO_TALENT_LEVELS.map(function(HERO_TALENT_LEVEL) { return ""+HERO_TALENT_LEVEL; });
 
 exports.HERO_JSON_SCHEMA.required = Object.keys(exports.HERO_JSON_SCHEMA.properties);
 exports.HERO_JSON_SCHEMA.required.remove("releaseDate");
 
-exports.HERO_MODIFICATIONS =
-{
-	"Crusader" :
-	[
-		{ set : { ratings : { damage : 3, utility : 6, survivability : 10, complexity : 4 } } }
-	],
-	"Chen" :
-	[
-		{ set : { releaseDate : "2014-09-10" } }
-	]
-};
-
+exports.HERO_JSON_SCHEMA.properties.talents.properties[10].minItems = 2;
+exports.HERO_JSON_SCHEMA.properties.talents.properties[10].maxItems = 2;
